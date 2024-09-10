3032e453dd430e430383e785ae35315dc1d7583d
Rian Mendonça do Vale <rianmvale@hotmail.com>
2024-09-10 18:15:03 -0300

    Alguns testes em Java

---
+++ 5.7/TesteSaida.java
@@ -0,0 +1,14 @@
+public class TesteSaida { 
+    public static void main(String[] args) {
+
+        System.out.print("Bem-vindo ao ");
+
+        System.out.println("mundo Java!");
+
+        System.out.print("Esta eh a ultima linha de ");
+
+        System.out.println("texto mostrada pelo programa.");
+
+    }
+
+}
