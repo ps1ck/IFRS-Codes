f8f1604177edc07652788a4b53d74dc51ff5582a
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-06-13 12:57:42 -0300

    1.3

---
+++ 1.3/EstatisticasCinema.java
@@ -0,0 +1,37 @@
+public class EstatisticasCinema {
+
+    public static void main(String[] args) {
+
+      int idade, somaIdades = 0, maiorIdade = -1, contEspectadores = 0;
+      int resOtimo = 0, resRuim = 0, resPessimo = 0;
+      char nota;
+      double mediaIdades = 0.0, percentualPessimo = 0.0;
+   
+      for(int cont = 1; cont <= 5; cont++) {
+           System.out.printf("--- ESPECTADOR %03d ---\n", cont);
+           System.out.printf("Idade: ");
+           idade = Integer.parseInt(System.console().readLine());
+
+           System.out.printf("Nota: ");
+           nota = System.console().readLine().charAt(0);
+
+           if(nota == 'A' || nota == 'a'){
+               resOtimo++;
+           }else if(nota == 'D' || nota == 'd'){
+               resRuim++;
+               somaIdades+= idade;
+           }else if(nota == 'E' || nota == 'e'){
+               resPessimo++;
+               if(idade > maiorIdade)
+                   maiorIdade = idade;
+           }
+           contEspectadores++;
+      }
+      percentualPessimo = ((resPessimo * 100)/contEspectadores);
+      mediaIdades = somaIdades / resRuim;
+      System.out.printf("Notas Ótimo = %d\n", resOtimo);
+      System.out.printf("Média Idades das notas ruins= %.2f\n", mediaIdades); 
+      System.out.printf("Percentual notas 'Péssimo' = %.2f %%\n", percentualPessimo);
+      System.out.printf("Maior idade a responder 'Péssimo' = %d\n", maiorIdade);
+   }
+}
